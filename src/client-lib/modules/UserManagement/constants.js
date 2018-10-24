4fb41d9fded7ba1ca6a825eaf822fd8d5c92637a
QianjinQin(秦前进) <WH1508031@wh.wistronits.com>
2018-10-24 18:18:57 +0800

    update projects

--- src/client-lib/modules/UserManagement/constants.js
+++
@@ -1,3 +0,0 @@
-export default {
-	NAME: 'USERMANAGEMENT_MODULE'
-};
--- src/client-lib/reduxStore/storeEnhancers.js
+++
@@ -1,8 +0,0 @@
-import { applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-
-const middleware = [thunk];
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export default composeEnhancers(applyMiddleware(...middleware));
