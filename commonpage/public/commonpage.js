c4d875f72d8b8a448fe21c96e2a0c2ba8e68549b
Alex Qin <qianjin.qin@qq.com>
2018-11-05 18:27:30 +0800

    update to reference relative files

---
+++ commonpage/public/commonpage.js
@@ -0,0 +1,11 @@
+window.CommonPageComponent = zoid.create({
+
+    // The html tag used to render my component
+
+    tag: 'commonpage-component',
+
+    // The url that will be loaded in the iframe or popup, when someone includes my component on their page
+
+    url: 'http://localhost:9000/',
+    dimensions: { width: '100%', height: '800px'}
+});
